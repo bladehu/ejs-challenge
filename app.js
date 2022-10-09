06a8f703fafa4e83f358a5b95fad5ade5ce0df17
Olivér Süle <sule.oliver91@gmail.com>
2022-10-09 14:04:32 +0200

    Add { MongoDB database Mongoose Connect database to MongoDB Atlas Save composed posts to MongoDB Atlas database Prepare deploying the website - port reconstruction Add button to reach /compose}

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,12 +19,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static("public"));
 
+mongoose.connect("mongodb+srv://admin-oliver:test123@cluster0.u1xemjr.mongodb.net/postsDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+const post1 = new Post({
+  title: "Home",
+  content: homeStartingContent
+});
+
+const defaultItems = [post1];
+
 app.get("/", function(req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    homeContent: posts,
+  Post.find({}, function(err, foundPosts){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        homeContent: foundPosts,
+      });
+    }
   });
 });
 
@@ -44,28 +68,36 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const newPost = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(newPost);
-  res.redirect("/");
-});
-
-app.get("/posts/:nextPost", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.nextPost);
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      res.render("post",{
-        recentPostTitle: post.title,
-        recentPostContent: post.content,
-      });
-    };
+  });
+  post.save(function(err){
+    if (!err){
+      res.redirect("/");
+    }
   });
 });
 
+app.get("/posts/:postId", function(req, res){
+  const requestedId = req.params.postId;
+  Post.findOne({_id: requestedId}, function(err, foundPost){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", {
+        recentPostTitle: foundPost.title,
+        recentPostContent: foundPost.content
+      });
+    }
+  });
+});
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
